acbec6040700b4dd263f18e783abca571f082dcc
Bastian Gebhardt <kolol@buzz-t.eu>
2023-06-10 13:34:23 +0200

    box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AwesomeMenuComponent } from './awesome-menu/awesome-menu.component';
 import { CharpaneComponent } from './charpane/charpane.component';
 import { ChatComponent } from './chat/chat.component';
+import { BoxComponent } from './core/box/box.component';
 import { LoginComponent } from './login/login.component';
 import { CampgroundComponent } from './main/campground/campground.component';
 import { CraftingComponent } from './main/crafting/crafting.component';
@@ -16,36 +17,36 @@ import { InventoryComponent } from './main/inventory/inventory.component';
 import { MainComponent } from './main/main.component';
 import { MapComponent } from './main/map/map.component';
 import { OptionsComponent } from './main/options/options.component';
+import { SkillSectionComponent } from './main/skills/skill-section/skill-section.component';
 import { SkillsComponent } from './main/skills/skills.component';
 import { TheBigMountainsComponent } from './main/the-big-mountains/the-big-mountains.component';
 import { TheDistantWoodsComponent } from './main/the-distant-woods/the-distant-woods.component';
-import { SkillSectionComponent } from './main/skills/skill-section/skill-section.component';
 
 @NgModule({
   bootstrap: [ AppComponent ],
   declarations: [
     AppComponent,
-    CharpaneComponent,
     AwesomeMenuComponent,
-    MainComponent,
-    ChatComponent,
+    BoxComponent,
     CampgroundComponent,
+    CharpaneComponent,
+    ChatComponent,
+    CraftingComponent,
+    InventoryComponent,
+    InventorySectionComponent,
+    LoginComponent,
+    MainComponent,
+    MapComponent,
+    OptionsComponent,
+    SkillSectionComponent,
+    SkillsComponent,
     TheBigMountainsComponent,
     TheDistantWoodsComponent,
-    InventoryComponent,
-    SkillsComponent,
-    CraftingComponent,
-    OptionsComponent,
-    MapComponent,
-    LoginComponent,
-    InventorySectionComponent,
-    SkillSectionComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    // FormsModule,
     ReactiveFormsModule,
   ],
   providers: [],
---
+++ src/app/core/box/box.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'kolol-box',
+  styleUrls: [ './box.component.scss' ],
+  templateUrl: './box.component.html',
+})
+export class BoxComponent {
+
+  @Input({ required: true })
+  public headline!: string;
+}
