d85e74b5571a2855b4893d64448f07f4e0f8de7d
Bastian Gebhardt <kolol@buzz-t.eu>
2023-06-13 09:46:20 +0200

    main map layout

--- src/app/main/map/map.component.ts
+++ src/app/main/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+import { Map } from './map.types';
 import { MapParserService } from '../../parser/map-parser.service';
 
 @Component({
@@ -9,11 +10,31 @@ import { MapParserService } from '../../parser/map-parser.service';
 })
 export class MapComponent {
 
+  @Input({ required: true })
+  public mapTiles: Map = [
+      [ 
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/newmap1new.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map2.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map3.gif',
+      ],
+      [ 
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map4a.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map4b.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map5.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map6.gif',
+      ],
+      [ 
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map7.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map8.gif',
+        'https://d2uyhvukfffg5a.cloudfront.net/otherimages/main/map9.gif',
+      ],
+    ];
+
   public constructor(private mapParserService: MapParserService) {
     //
   }
 
-  // public ngOnInit(): void {
-  //   // this.mapParserService.
-  // }
+  public tileClicked(x: number, y: number): void {
+    console.log('tile clicked: ', x, y);
+  }
 }
---
+++ src/app/main/map/map.types.ts
@@ -0,0 +1,5 @@
+export type Map = [
+    [string, string, string],
+    [string, string, string, string],
+    [string, string, string],
+];
