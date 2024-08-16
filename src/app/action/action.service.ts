6bf0c104166900062e92cc84fcffb3a2d905df7a
Bastian Gebhardt <kolol@buzz-t.eu>
2024-08-16 23:54:03 +0200

    fix path for postPath in action service

--- src/app/action/action.service.ts
+++ src/app/action/action.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 import { AbstractActionService } from './abstract-action.service';
 import { Equipment } from '../../shared/inventory.types';
@@ -63,12 +62,12 @@ export class ActionService extends AbstractActionService {
    * targetPlayer 0 means casting to yourself
    */
   public castSkill({ pwd, skillId, quantity = 1, targetPlayer = 0 }: CastSkillParams): Observable<unknown> {
-    return this.postPath(`${environment.backendDomain}/skill`, pwd, { quantity: quantity.toString(), skillId, targetPlayer: targetPlayer.toString() });
+    return this.postPath('/skill', pwd, { quantity: quantity.toString(), skillId, targetPlayer: targetPlayer.toString() });
   }
 
   public useItem({ action, itemId, pwd, quantity, which }: UseItemParams): void {
     // TODO: does "quantity: ''" work?
-    this.postPath(`${environment.backendDomain}/item/use`, pwd, { action, itemId, quantity: quantity ? quantity.toString() : '', which: which.toString() }).subscribe((success) => {
+    this.postPath('/item/use', pwd, { action, itemId, quantity: quantity ? quantity.toString() : '', which: which.toString() }).subscribe((success) => {
       console.log('success item use;', success);
     });
   }
@@ -79,7 +78,7 @@ export class ActionService extends AbstractActionService {
   public equipItem({ isOffhand, itemId, which, pwd }: EquipItemParams): void {
     console.log('equip item');
 
-    this.postPath(`${environment.backendDomain}/item/equip`, pwd, { itemId, offhand: isOffhand.toString(), which: which.toString() }).subscribe((success) => {
+    this.postPath('/item/equip', pwd, { itemId, offhand: isOffhand.toString(), which: which.toString() }).subscribe((success) => {
       console.log('equip item: ', success);
     });
   }
@@ -87,7 +86,7 @@ export class ActionService extends AbstractActionService {
   public unequipItem({ equipmentSection, pwd }: UnequipItemParams): void {
     console.log('unequip item: ', equipmentSection);
 
-    this.postPath(`${environment.backendDomain}/item/unequip`, pwd, { section: equipmentSection }).subscribe((success) => {
+    this.postPath('/item/unequip', pwd, { section: equipmentSection }).subscribe((success) => {
       console.log('unequip item: ', success);
     });
 
@@ -96,7 +95,7 @@ export class ActionService extends AbstractActionService {
   public buyItem({ pwd, quantity, row, shop }: BuyItemParams): void {
     console.log('buyItem: ', pwd, quantity, row, shop);
 
-    this.postPath(`${environment.backendDomain}/item/buy`, pwd, {  quantity, row, shop })
+    this.postPath('/item/buy', pwd, {  quantity, row, shop })
       .pipe(
         map(result => this.resultsParserService.parseHtml(result)),
       )
